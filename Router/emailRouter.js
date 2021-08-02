16ece2650c4a023c55d0c7658f37a68edbd60f33
SMRsaimon <smrsaimon960@gmail.com>
2021-08-02 05:22:31 -0700

    all are done and ready to build

--- Router/emailRouter.js
+++ Router/emailRouter.js
@@ -1,13 +1,42 @@
-const express=require("express")
-const emailRouter=express.Router()
+const express = require("express");
+const emailRouter = express.Router();
 
+const nodemailer = require("nodemailer");
 
+// api: http://localhost:5000/sendEmail/emailsend/data/post
 
-emailRouter.post("/emailsend/data/post",(ewq,res,next)=>{
+emailRouter.post("/emailsend/data/post", (req, res, next) => {
+  const from = req.body.email;
+  const message = req.body.message;
+  const number = req.body.number;
+  const name = req.body.name;
 
-     const from=req.body.email;
-     const subject=req.body.subject;
-     const detals=req.body.details;
-     
+  let transporter = nodemailer.createTransport({
+    host: process.env.HOST_NAME,
+    port: process.env.HOST_PORT,
+    secure: true,
+    auth: {
+      user: process.env.RECEIVER_EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-})
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: from,
+    to: process.env.RECEIVER_EMAIL,
+    subject: name,
+    text: message,
+  };
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(502).send(error);
+    }
+    if (info) {
+      res.send(" message successfully send ");
+    }
+  });
+});
+
+module.exports = emailRouter;
--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const multer =require("multer")
 const dotenv=require("dotenv")
 const path = require("path");
 const app = express();
-const db=require("./DataBaseConnection")
-const nodemailer = require('nodemailer');
+
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
@@ -27,39 +26,8 @@ app.use("/admin/login",  logInRoute)
 app.use("/adminPanel", authCheck, adminPanelRouter)
 app.use("/adminInformation", adminInfoRoute)
 app.use("/bioInfo", bioInfoRouter)
-// app.use("/sendEmail", emailRouter)
+ app.use("/sendEmail", emailRouter)
 
-let transporter = nodemailer.createTransport({
-
-  
-  host: "mail.reyadabedin.com",
-  port: 465,
-  secure: true, 
-  auth: {
-      user: 'info@reyadabedin.com', 
-      pass: 'info@reyad!@#$'  
-  },
-  
-});
-
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: 'saimoncse333@gmail.com', 
-    to: 'info@reyadabedin.com', 
-    subject: 'Node Contact Request', 
-    text: 'test data send', 
-    
-};
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);   
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-   
-});
 
 //custom  error handeling
 app.use((err, req, res, next) => {
